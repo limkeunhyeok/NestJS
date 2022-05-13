4d856c1b43d6030790ac8b478a8046a406624f05
limkeunhyeok <gorloom6425@naver.com>
2022-05-13 17:36:03 +0900

    nest cqrs

--- user-service/src/users/users.controller.ts
+++ user-service/src/users/users.controller.ts
@@ -3,51 +3,69 @@ import {
   Controller,
   Get,
   Headers,
+  Inject,
+  Logger,
+  LoggerService,
   Param,
   Post,
   Query,
   UseGuards,
 } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { AuthGuard } from 'src/auth.guard';
 import { AuthService } from 'src/auth/auth.service';
+import { CreateUserCommand } from './command/create-user.command';
+import { LoginCommand } from './command/login.command';
+import { VerifyEmailCommand } from './command/verify-email.command';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
+import { GetUserInfoQuery } from './query/get-user-info.query';
 import { UserInfo } from './userInfo';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(
-    private usersService: UsersService,
-    private authService: AuthService,
+    private commandBus: CommandBus,
+    private queryBus: QueryBus,
+    @Inject(Logger) private readonly logger: LoggerService,
   ) {}
 
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
     const { name, email, password } = dto;
-    await this.usersService.createUser(name, email, password);
+
+    const command = new CreateUserCommand(name, email, password);
+
+    return this.commandBus.execute(command);
   }
 
   @Post('/email-verify')
   async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
     const { signupVerifyToken } = dto;
-    return await this.usersService.verifyEmail(signupVerifyToken);
+
+    const command = new VerifyEmailCommand(signupVerifyToken);
+
+    return await this.commandBus.execute(command);
   }
 
   @Post('/login')
   async login(@Body() dto: UserLoginDto): Promise<string> {
     const { email, password } = dto;
-    return await this.usersService.login(email, password);
+
+    const command = new LoginCommand(email, password);
+
+    return this.commandBus.execute(command);
   }
 
   @UseGuards(AuthGuard)
   @Get('/:id')
   async getUserInfo(
-    @Headers() headers: any,
     @Param('id') userId: string,
   ): Promise<UserInfo> {
-    console.log("GHJFDSA")
-    return await this.usersService.getUserInfo(userId);
+    const getUserInfoQuery = new GetUserInfoQuery(userId);
+
+    return this.queryBus.execute(getUserInfoQuery);
   }
 }
